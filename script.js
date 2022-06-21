6ce604040c0745199970fd1671e2f4bc3dc208c5
kene4500 <42975728+kene4500@users.noreply.github.com>
2022-06-22 00:07:48 +0100

    The simple calculator is complete

---
+++ script.js
@@ -0,0 +1,34 @@
+alert("This is a simple calculator\nType your first number\nThen your Second number\nThen choose the operator you want to use such as +,-,*,/");
+myCalculator();
+
+function myCalculator(){
+    let firstNum, secondNum, operator;
+    firstNum = prompt("Type your first Number");
+    secondNum = prompt("Type your second Number");
+    operator = prompt("Type you operator");
+
+    if (operator=="+"){
+        result = Number(firstNum) + Number(secondNum);
+        alert(result);
+    }
+
+    else if (operator=="-"){
+        result = Number(firstNum) - Number(secondNum);
+        alert(result);
+    }
+
+    else if (operator=="*" || operator=="x" || operator=="X"){
+        result = Number(firstNum) * Number(secondNum);
+        alert(result);
+    }
+
+    else if (operator=="/"){
+        result = Number(firstNum) / Number(secondNum);
+        alert(result);
+    }
+
+    else{
+        alert("There was an error in your input!!");
+        location.reload();
+    }
+}
